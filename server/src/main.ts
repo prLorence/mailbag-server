76ae1c0e0eb2bb2e862ef3e36a0a7d1a6b736242
prLorence <paulorencereyes@gmail.com>
2023-02-04 19:28:09 +0800

    feat: get all messages, list a message with id, delete a message with id, post a message

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,11 @@
-import path from "path";
+import path, { parse } from "path";
 import express, { Express, NextFunction, Request, response, Response } from "express";
 import { serverInfo } from "./ServerInfo";
 import * as IMAP from "./IMAP";
 import * as SMTP from "./SMTP";
 import * as Contacts from "./Contacts";
 import { IContact } from "./Contacts";
+import { inflateRaw } from "zlib";
 
 const app: Express = express();
 
@@ -24,6 +25,9 @@ app.use(function(inRequest : Request, inResponse: Response, inNext: NextFunction
   inNext();
 })
 
+// ! endpoints
+
+// visiting a site triggers a GET method, lists all mailboxes
 app.get("/mailboxes", 
   async (inRequest : Request, inResponse : Response, inNext : NextFunction) => {
     try {
@@ -36,6 +40,65 @@ app.get("/mailboxes",
   }
 )
 
+// gets all messages
+app.get("/mailboxes/:mailbox", 
+    async (inRequest: Request, inResponse: Response, inNext: NextFunction) => {
+      try {
+        const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+        const messages: IMAP.IMessage[] = await imapWorker.listMessages({
+          mailbox: inRequest.params.mailbox
+        });
+        inResponse.json(messages);
+      } catch (inError) {
+        inResponse.send("error");
+      }
+  }
+) 
 
+// get message inside a mailbox with id
+app.get("/messages/:mailbox/:id", 
+    async (inRequest: Request, inResponse: Response, inNext: NextFunction) => {
+      try {
+        const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+        const messageBody: string | undefined = await imapWorker.getMessageBody({
+          mailbox: inRequest.params.mailbox,
+          id: parseInt(inRequest.params.id, 10)
+        })
+        inResponse.send(messageBody);
+      } catch(inError) {
+        inResponse.send("error");
+      }
+    }
+)
+
+// delete message in mailbox with id
+app.delete("/messages/:mailbox/:id", 
+    async (inRequest: Request, inResponse: Response, inNext: NextFunction) => {
+      try {
+        const imapWorker: IMAP.Worker = new IMAP.Worker(serverInfo);
+        await imapWorker.deleteMessage({
+          mailbox: inRequest.params.mailbox,
+          id: parseInt(inRequest.params.id, 10)
+        });
+        inResponse.send("ok");
+      } catch (inError) {
+        inResponse.send("error");
+      }
+    }
+) 
+
+// create message in message
+app.post("/messages", 
+    async (inRequest: Request, inResponse: Response) => {
+      try {
+        const smtpWorker: SMTP.Worker = new SMTP.Worker(serverInfo);
+        await smtpWorker.sendMessage(inRequest.body);
+        inResponse.send("ok");
+      } catch (inError) {
+      inResponse.send("error");
+      }
+    }
+);
+ 
 
 
